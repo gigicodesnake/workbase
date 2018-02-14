e7f871860ace9f82ff26a7b182f1930f1dea6b39
gigi <gigicodesnake@gmail.com>
2018-02-14 15:35:33 +0100

    wip

--- Demo/iiiMain.py
+++ Demo/iiiMain.py
@@ -4,6 +4,9 @@ Created on 26 Jan 2018
 @author: chris
 '''
 from kivy.app import App
+from kivy.clock import Clock
+from kivy.storage.jsonstore import JsonStore
+
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
@@ -13,12 +16,27 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.behaviors.focus import FocusBehavior
+
 import db_methods
+
 from email.policy import default
+#from os.path import join
+from bcrypt import hashpw, gensalt   
+    
 
 #the manager for the various screens
 class Screen_manager(ScreenManager):
     pass
+class Login_screen(Screen):
+    def try_to_log(self):
+        if self.ids.name_input.text == '' or self.ids.pass_input.text == '':
+            popup = Popup(title='Input error', content=Label(text='Name field and password field cannot be left blank.\nPlease click outside this popup to enter a new value.'),
+            size_hint= (0.5, 0.5))
+            popup.open(0)
+            return True
+        else:
+            db_methods.log_attempt(self.ids.name_input.text, self.ids.pass_input.text)
 
 # this is the current opening screen, to be preceded by a login 
 class Main_screen(Screen):
@@ -65,7 +83,24 @@ class Lab_orders_input_screen(Screen):
             popup.open(0)
             return True     
         
-        db_methods.input_into_orders_table(input_list)       
+        db_methods.input_into_orders_table(input_list)
+        
+        self.ids['name_field'].text = ''
+        self.ids['catalouge_field'].text = ''
+        self.ids['cas_field'].text = ''
+        self.ids['manufacturer_field'].text = ''
+        self.ids['supplier_field'].text = ''
+        self.ids['price_field'].text = '0'
+        self.ids['quantity_field'].text = ''
+        self.ids['ordered_field'].text = '0'
+        self.ids['user_field'].text = ''
+        self.ids['reference_field'].text = ''
+        self.ids['order_open_toggle'].state = 'down'
+        self.ids['order_partial_toggle'].state = 'normal'
+        self.ids['order_closed_toggle'].state = 'normal'
+        self.ids['notes_field'].text = ''
+
+        
 class Lab_stock_input_screen(Screen):
     
     x_hint_labels = 0.25
@@ -93,7 +128,11 @@ class Lab_operations_buttons(BoxLayout):
         pass
 class Lab_tables_list(BoxLayout):
     pass
-class FloatInput(TextInput):
+class TextField(TextInput):
+    
+    write_tab = False
+    
+class FloatInput(TextField):
     
     default_value = '0'
    
